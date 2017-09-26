901710e354f7391e92dc91f6b5df130f6fc5871f
Dabutou <31713418+Dabutou@users.noreply.github.com>
2017-09-25 20:05:54 -0500

    Add files via upload
    Hardware for Teleop 9/25/17 without lift or sensors

--- Hardware.java
+++ Hardware.java
@@ -52,42 +52,42 @@ public class Hardware {
         frontRightMotor = hwMap.dcMotor.get("right_front");
         backLeftMotor = hwMap.dcMotor.get("left_back");
         backRightMotor = hwMap.dcMotor.get("right_back");
-        liftMotor = hwMap.dcMotor.get("lift");
+        //liftMotor = hwMap.dcMotor.get("lift");
 
         // Initialize Motors
         frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        liftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        //liftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         // ******MAY CHANGE *******  Fix Forward/Reverse under testing
         frontLeftMotor.setDirection(DcMotor.Direction.FORWARD);
         frontRightMotor.setDirection(DcMotor.Direction.FORWARD);
-        backLeftMotor.setDirection(DcMotor.Direction.REVERSE);
-        backRightMotor.setDirection(DcMotor.Direction.REVERSE);
-        liftMotor.setDirection(DcMotor.Direction.FORWARD);
+        backLeftMotor.setDirection(DcMotor.Direction.FORWARD);
+        backRightMotor.setDirection(DcMotor.Direction.FORWARD);
+        //liftMotor.setDirection(DcMotor.Direction.FORWARD);
 
         frontLeftMotor.setPower(0);
         frontRightMotor.setPower(0);
         backLeftMotor.setPower(0);
         backRightMotor.setPower(0);
-        liftMotor.setPower(0);
+        //liftMotor.setPower(0);
 
         // May use RUN_USING_ENCODERS if encoders are installed
         frontLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         frontRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         backLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         backRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        liftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        //liftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         // Define servos
-        leftLiftServo = hwMap.servo.get("left_lift");
-        rightLiftServo = hwMap.servo.get("right_lift");
+        //leftLiftServo = hwMap.servo.get("left_lift");
+        //rightLiftServo = hwMap.servo.get("right_lift");
 
         // Initialize servos
-        leftLiftServo.setPosition(LEFT_LIFT_CLOSE);
-        rightLiftServo.setPosition(RIGHT_LIFT_CLOSE);
+        //leftLiftServo.setPosition(LEFT_LIFT_CLOSE);
+        //rightLiftServo.setPosition(RIGHT_LIFT_CLOSE);
 
         // Define sensors
 
