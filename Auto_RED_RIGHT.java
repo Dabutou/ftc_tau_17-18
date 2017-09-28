d0eca4d1cdd13bac2c3346d92e1c372e8cb4c3c8
abhinav9225 <31747445+abhinav9225@users.noreply.github.com>
2017-09-27 20:15:58 -0500

    Add files via upload

--- Auto_RED_RIGHT.java
+++ Auto_RED_RIGHT.java
@@ -4,6 +4,16 @@ package org.firstinspires.ftc.teamcode;
  * Created by Lance He on 9/20/2017.
  */
 
-public class Auto_RED_RIGHT{
+public class Auto_RED_RIGHT extends Hardware{
     Hardware h = new Hardware();
+
+    private double speed1; // each speed is for different motors
+
+    private double speed2;
+
+    private double speed3;
+
+    private double speed4;
+
+    private static final double orientation = 0.0;
 }
