d012081ca3544f438679981d317c1964f8380744
Dabutou <31713418+Dabutou@users.noreply.github.com>
2017-10-04 20:27:21 -0500

    Add files via upload
    Don't know if it for sure works

--- Hardware.java
+++ Hardware.java
@@ -1,15 +1,42 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.adafruit.AdafruitBNO055IMU;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.configuration.DistributorInfo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.robotcore.external.Func;
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
+import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
+import org.firstinspires.ftc.robotcore.external.navigation.VuMarkInstanceId;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+
+import java.util.Locale;
+
 /**
  * Created by Lance He on 9/16/2017.
  */
 
-public class Hardware {
+
+public class Hardware{
 
     // Motor variable names
     public DcMotor frontLeftMotor = null;
@@ -24,11 +51,12 @@ public class Hardware {
     public Servo rightLiftServo = null;
 
     // Sensor variable names
-
+    BNO055IMU imu;
 
     // Other variable names
     HardwareMap hwMap = null;
     private ElapsedTime period = new ElapsedTime();
+
 
     // Constant variable names
     public static final double LEFT_LIFT_OPEN = 0;
@@ -97,11 +125,24 @@ public class Hardware {
         //rightLiftServo.setPosition(RIGHT_LIFT_CLOSE);
 
         // Define sensors
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibrafion sample opmode
+        parameters.loggingEnabled      = true;
+        parameters.loggingTag          = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
 
+        imu = hwMap.get(BNO055IMU.class, "imu");
         // Initialize sensors
-
+        imu.initialize(parameters);
 
     }
+
+    public double getTime(){
+        return period.time();
+    }
+
 }
 
 
